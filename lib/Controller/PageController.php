4585656a649b0060d66257430c5b509362f79d53
Vinicius Reis <vinicius.reis@nextcloud.com>
2022-04-27 18:39:11 -0300

    ✨ load scripts and styles

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -4,16 +4,25 @@ declare(strict_types=1);
 
 namespace OCA\WifiPassword\Controller;
 
+use OC\Security\CSP\ContentSecurityPolicyNonceManager;
+use OCP\App\IAppManager;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http\TemplateResponse;
 use OCP\IRequest;
 use OCA\WifiPassword\AppInfo\Application;
-use OCP\Util;
 
 class PageController extends Controller {
-	public function __construct(string $appName,
-								IRequest $request) {
+	private IAppManager $appManager;
+	private ContentSecurityPolicyNonceManager $nonceManager;
+
+	public function __construct(string      $appName,
+								IRequest    $request,
+								IAppManager $appManager,
+								ContentSecurityPolicyNonceManager $nonceManager) {
 		parent::__construct($appName, $request);
+
+		$this->appManager = $appManager;
+		$this->nonceManager = $nonceManager;
 	}
 
 	/**
@@ -21,7 +30,11 @@ class PageController extends Controller {
 	 * @NoCSRFRequired
 	 */
 	public function index(): TemplateResponse {
-        Util::addScript(Application::APP_ID, "assets/page-main");
-		return new TemplateResponse(Application::APP_ID, "page-main");
+		$nonce = $this->nonceManager->getNonce();
+		$base = $this->appManager->getAppWebPath(Application::APP_ID);
+		$script = $base . '/js/assets/page-main.js';
+		$style = $base . '/js/assets/page-main.css';
+
+		return new TemplateResponse(Application::APP_ID, "page-main", compact('script', 'nonce', 'style'));
 	}
 }
--- templates/page-main.php
+++ templates/page-main.php
@@ -1,1 +1,3 @@
+<link rel="stylesheet" href="<?= $style ?>" />
+<script type="module" src="<?= $script ?>" defer nonce="<?= $nonce ?>"></script>
 <div id="wifi-pass-content"></div>
